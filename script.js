78dbbf8caea206ddbb908acab388bee23c7e1481
J-Quentin <82309920+J-Quentin@users.noreply.github.com>
2021-04-11 14:25:59 +0200

    changement de place du script

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-document.write(d3.version);
+let corps = d3.select("body");
+//ajouter un canevas à la page
+let canevas = corps.append("svg")
+    .attr("width". 600)
+    .attr("height". 600);
 
+//
+
+canevas.append("circle")
+    .attr("cx",60)
+    .attr("cy",60)
+    .attr("r",40);
