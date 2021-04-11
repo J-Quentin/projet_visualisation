1824c2b4f8ef01613e07294c0420ee9e5642794c
J-Quentin <82309920+J-Quentin@users.noreply.github.com>
2021-04-11 14:36:48 +0200

    création de cercles

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+let donnees = [30,60,120,340]
+
 let corps = d3.select("body");
 //ajouter un canevas à la page
 let canevas = corps.append("svg")
-    .attr("width". 600)
-    .attr("height". 600);
+    .attr("width", 600)
+    .attr("height", 600);
 
 //cercle en exemple
-let mon_cercle = canevas.append("circle")
-    .attr("cx",60)
-    .attr("cy",60)
-    .attr("r",40);
+//let mon_cercle = canevas.append("circle")
+//   .attr("cx",60)
+//    .attr("cy",60)
+//    .attr("r",40);
+
+//ajouter autant de cercles que de données
+canevas.selectAll("circle")
+    .data(donnees)
+    .enter()
+    .append("circle")
+        .attr("cx",(d => d))
+        .attr("cy",300)
+        .attr("r",d => d)
+        .style("opacity", 0,5);
